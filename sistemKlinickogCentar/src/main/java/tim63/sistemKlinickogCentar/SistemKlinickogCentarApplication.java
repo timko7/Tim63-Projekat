637d8194e3322bf9035f295fa82d305485d94dc5
Timotije Sabo <57005164+timko7@users.noreply.github.com>
2019-11-08 14:08:32 +0100

    Inicijalni komit.

---
+++ sistemKlinickogCentar/src/main/java/tim63/sistemKlinickogCentar/SistemKlinickogCentarApplication.java
@@ -0,0 +1,13 @@
+package tim63.sistemKlinickogCentar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemKlinickogCentarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemKlinickogCentarApplication.class, args);
+	}
+
+}
