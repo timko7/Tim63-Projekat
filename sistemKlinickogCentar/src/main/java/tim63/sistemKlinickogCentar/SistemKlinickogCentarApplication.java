8954f6b361984900748c00755b145c43dbbaaf5e
Timotije Sabo <57005164+timko7@users.noreply.github.com>
2020-02-03 22:09:30 +0100

    Implementirano dodela sale pregledima/pravljenje pregleda ukoliko admin do kraja dana ne odgovori na njih.

--- sistemKlinickogCentar/src/main/java/tim63/sistemKlinickogCentar/SistemKlinickogCentarApplication.java
+++ sistemKlinickogCentar/src/main/java/tim63/sistemKlinickogCentar/SistemKlinickogCentarApplication.java
@@ -5,10 +5,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @EnableTransactionManagement
+/*
+ * Ukljucivanje podrske za raspored izvrsavanja.
+ */
+@EnableScheduling
 public class SistemKlinickogCentarApplication {
 
     public static void main(String[] args) {
