4f7cbe27318439d71df7d8875591acc2caff22d6
Timotije Sabo <57005164+timko7@users.noreply.github.com>
2020-02-05 01:09:55 +0100

    Prepravljeno da kad pacijent oceni lekara odma se raduna njegova srednja ocena.

--- sistemKlinickogCentar/src/main/java/tim63/sistemKlinickogCentar/service/OcenaService.java
+++ sistemKlinickogCentar/src/main/java/tim63/sistemKlinickogCentar/service/OcenaService.java
@@ -2,6 +2,7 @@ package tim63.sistemKlinickogCentar.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import tim63.sistemKlinickogCentar.model.Lekar;
 import tim63.sistemKlinickogCentar.model.Ocena;
 import tim63.sistemKlinickogCentar.repository.OcenaRepository;
 
@@ -13,6 +14,9 @@ public class OcenaService implements OcenaInterface {
 
     @Autowired
     private OcenaRepository repositoryOcena;
+
+    @Autowired
+    private LekarService lekarService;
 
     @Override
     public Collection<Ocena> findAll() {
@@ -29,6 +33,18 @@ public class OcenaService implements OcenaInterface {
         Ocena ret = new Ocena();
         ret.setOcena(pregled.getOcena());
         ret.setIdLekara(pregled.getIdLekara());
+
+        System.out.println("Ocena: ocena->" + ret.getOcena() + " idLekara->" + ret.getIdLekara());
+        Lekar lekar = lekarService.findById(pregled.getIdLekara());
+
+        List<Ocena> oceneLekara = findByIdLekara(lekar.getId());
+        double zbir = 0;
+        for(Ocena ocena : oceneLekara){
+            zbir += ocena.getOcena();
+        }
+        lekar.setSrednjaOcena(zbir / oceneLekara.size());
+        lekarService.update(lekar);
+
         ret = repositoryOcena.save(ret);
         return ret;
     }
